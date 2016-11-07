890b66afbd118854859b0b1128f399b3977b14ea
Tomás Tecce <tomastecce@gmail.com>
2016-11-07 18:14:56 -0300

    Completed programs for chapter 7.

---
+++ equalize.py
@@ -0,0 +1,18 @@
+import numpy as np
+import argparse
+import cv2
+
+# Parse the command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# Load the image and convert to grayscale
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Perform histogram equalisation
+eq = cv2.equalizeHist(image)
+
+cv2.imshow("Histogram Equalization", np.hstack([image, eq]))
+cv2.waitKey(0)
---
+++ histogram_with_mask.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import argparse
+import cv2
+
+# Define a convenience function to avoid repeating code
+def plot_histogram(image, title, mask=None):
+    chans = cv2.split(image)
+    colors = ("b", "g", "r")
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
+
+    for (chan, color) in zip(chans, colors):
+        hist = cv2.calcHist([chan], [0], mask, [256], [0, 256])
+        plt.plot(hist, color=color)
+        plt.xlim([0, 256])
+    plt.show()
+
+# Parse the command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# Load image and plot a histogram for each channel
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+plot_histogram(image, "Histogram for Original Image")
+cv2.waitKey(0)
+
+# Create a mask
+mask = np.zeros(image.shape[:2], dtype="uint8")
+cv2.rectangle(mask, (15, 15), (130, 100), 255, -1)
+cv2.imshow("Mask", mask)
+
+masked = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Applying the Mask", masked)
+plot_histogram(image, "Histogram for Masked Image", mask=mask)
+cv2.waitKey(0)
+
